baeafce1977aeae4afcb553e7d43fa0cf6e48a89
tolgahanbora <tolgahanbora@outlook.com>
2023-06-13 17:53:54 +0300

    fix next.config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['bayut-production.s3.eu-central-1.amazonaws.com'],
-    loader: "imgix", // veya "default" olarak ayarlayın
-    path: "/", // veya resimlerinizin bulunduğu klasörü belirtin
+    loader: "default", // veya "default" olarak ayarlayın
+
   }
 }
 
